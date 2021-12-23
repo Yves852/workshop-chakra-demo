54941d7c970873cf72e4c26e344199aebb219cc8
yves <thirion.yves@gmail.com>
2021-12-23 16:19:54 +0100

    Cart#1 added top, updated spacing & padding

--- src/sections/cart.jsx
+++ src/sections/cart.jsx
@@ -1,13 +1,24 @@
-import { VStack } from "@chakra-ui/react";
+import { VStack, Heading, Text, Button } from "@chakra-ui/react";
 
 const Cart = () => (
   <VStack
     w={"full"}
     h={"full"}
-    spacing={10}
+    spacing={6}
+    p={10}
     alignItems={"flex-start"}
     bg="gray.200"
-  ></VStack>
+  >
+    <VStack w={"full"} spacing={3} alignItems={"flex-start"}>
+      <Heading size={"2xl"}>Your cart</Heading>
+      <Text>
+        If price is too hard on your eyes,{" "}
+        <Button variant={"link"} colorScheme={"black"}>
+          try changing the theme.
+        </Button>
+      </Text>
+    </VStack>
+  </VStack>
 );
 
 export default Cart;
