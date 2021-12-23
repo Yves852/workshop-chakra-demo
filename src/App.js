fca966f740600d350b9a9fafdee3e7d3b2b70679
yves <thirion.yves@gmail.com>
2021-12-23 10:52:19 +0100

    Created basic Details.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container, Flex } from "@chakra-ui/react";
+import Details from "./sections/details";
 
 const App = () => (
   <Container maxW={"container.xl"} p={0}>
     <Flex h={"100vh"} py={20}>
-      <p>Learn React with Chakra-ui</p>
+      <Details />
     </Flex>
   </Container>
 );
---
+++ src/sections/details.jsx
@@ -0,0 +1,10 @@
+import { VStack, Heading, Text } from "@chakra-ui/react";
+
+const Details = () => (
+  <VStack>
+    <Heading>Your details</Heading>
+    <Text>If you already have an account, click here to login.</Text>
+  </VStack>
+);
+
+export default Details;
