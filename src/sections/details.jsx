d8103a52f731aa5e6bbe2c24dc8b41ed373c3854
yves <thirion.yves@gmail.com>
2021-12-23 11:39:01 +0100

    Updated Details section tag

--- src/sections/details.jsx
+++ src/sections/details.jsx
@@ -1,7 +1,14 @@
 import { VStack, Heading, Text } from "@chakra-ui/react";
 
 const Details = () => (
-  <VStack>
+  <VStack
+    as={"section"}
+    w={"full"}
+    h={"full"}
+    p={10}
+    spacing={10}
+    alignItems={"flex-start"}
+  >
     <Heading>Your details</Heading>
     <Text>If you already have an account, click here to login.</Text>
   </VStack>
