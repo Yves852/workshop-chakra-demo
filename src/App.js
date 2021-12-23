466a50925edd48e5ecf9bab6ad468afdc3163c14
yves <thirion.yves@gmail.com>
2021-12-23 12:29:34 +0100

    Created base of cart.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Flex } from "@chakra-ui/react";
 import Details from "./sections/details";
+import Cart from "./sections/cart";
 
 const App = () => (
   <Container maxW={"container.xl"} p={0}>
     <Flex h={"100vh"} py={20}>
       <Details />
+      <Cart />
     </Flex>
   </Container>
 );
---
+++ src/sections/cart.jsx
@@ -0,0 +1,13 @@
+import { VStack } from "@chakra-ui/react";
+
+const Cart = () => (
+  <VStack
+    w={"full"}
+    h={"full"}
+    spacing={10}
+    alignItems={"flex-start"}
+    bg="gray.200"
+  ></VStack>
+);
+
+export default Cart;
