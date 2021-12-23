1a052e170efe6af4d7ca57b7bfeba08d12eb19bc
yves <thirion.yves@gmail.com>
2021-12-23 14:16:01 +0100

    Form #1 f&l name & adress & city

--- src/sections/details.jsx
+++ src/sections/details.jsx
@@ -1,4 +1,13 @@
-import { VStack, Heading, Text } from "@chakra-ui/react";
+import {
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 const Details = () => (
   <VStack
@@ -13,6 +22,32 @@ const Details = () => (
       <Heading size={"2xl"}>Your details</Heading>
       <Text>If you already have an account, click here to login.</Text>
     </VStack>
+    <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+      <GridItem colSpan={1}>
+        <FormControl>
+          <FormLabel>First Name</FormLabel>
+          <Input placeholder="John" />
+        </FormControl>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <FormControl>
+          <FormLabel>Last Name</FormLabel>
+          <Input placeholder="Doe" />
+        </FormControl>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <FormControl>
+          <FormLabel>Adress</FormLabel>
+          <Input placeholder="Nlvd. Broken Dreams 21" />
+        </FormControl>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <FormControl>
+          <FormLabel>City</FormLabel>
+          <Input placeholder="San Francisco" />
+        </FormControl>
+      </GridItem>
+    </SimpleGrid>
   </VStack>
 );
 
