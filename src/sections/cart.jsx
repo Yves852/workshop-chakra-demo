48a9a7c5fcf898527b65cc24dc4064017141d34b
yves <thirion.yves@gmail.com>
2021-12-27 10:24:23 +0100

    Added return statement

--- src/sections/cart.jsx
+++ src/sections/cart.jsx
@@ -10,62 +10,64 @@ import {
   AspectRatio,
 } from "@chakra-ui/react";
 
-const Cart = () => (
-  <VStack
-    w={"full"}
-    h={"full"}
-    spacing={6}
-    p={10}
-    alignItems={"flex-start"}
-    bg="gray.200"
-  >
-    <VStack w={"full"} spacing={3} alignItems={"flex-start"}>
-      <Heading size={"2xl"}>Your cart</Heading>
-      <Text>
-        If price is too hard on your eyes,{" "}
-        <Button variant={"link"} colorScheme={"black"}>
-          try changing the theme.
-        </Button>
-      </Text>
-      <HStack spacing={6} alignItems={"center"} w={"full"}>
-        <AspectRatio ratio={1} w={24}>
-          <Img src={"./board.png"} alt="Board image" />
-        </AspectRatio>
-        <Stack
-          spacing={0}
-          w={"full"}
-          direction={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
-          <VStack>
-            <Heading size={"sm"}>Penny board</Heading>
-            <Text>PNYCOMP27541</Text>
-          </VStack>
-          <Heading size={"sm"}>$119.00</Heading>
-        </Stack>
-      </HStack>
-      <VStack spacing={4} alignItems={"stretch"} w={"full"}>
-        <HStack justifyContent={"space-between"}>
-          <Text>Subtotal</Text>
-          <Heading size={"sm"}>$119.00</Heading>
+const Cart = () => {
+  return (
+    <VStack
+      w={"full"}
+      h={"full"}
+      spacing={6}
+      p={10}
+      alignItems={"flex-start"}
+      bg="gray.200"
+    >
+      <VStack w={"full"} spacing={3} alignItems={"flex-start"}>
+        <Heading size={"2xl"}>Your cart</Heading>
+        <Text>
+          If price is too hard on your eyes,{" "}
+          <Button variant={"link"} colorScheme={"black"}>
+            try changing the theme.
+          </Button>
+        </Text>
+        <HStack spacing={6} alignItems={"center"} w={"full"}>
+          <AspectRatio ratio={1} w={24}>
+            <Img src={"./board.png"} alt="Board image" />
+          </AspectRatio>
+          <Stack
+            spacing={0}
+            w={"full"}
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <VStack>
+              <Heading size={"sm"}>Penny board</Heading>
+              <Text>PNYCOMP27541</Text>
+            </VStack>
+            <Heading size={"sm"}>$119.00</Heading>
+          </Stack>
         </HStack>
-        <HStack justifyContent={"space-between"}>
-          <Text>Shipping</Text>
-          <Heading size={"sm"}>$19.99</Heading>
-        </HStack>
-        <HStack justifyContent={"space-between"}>
-          <Text>Taxes (estimated)</Text>
-          <Heading size={"sm"}>$23.80</Heading>
-        </HStack>
+        <VStack spacing={4} alignItems={"stretch"} w={"full"}>
+          <HStack justifyContent={"space-between"}>
+            <Text>Subtotal</Text>
+            <Heading size={"sm"}>$119.00</Heading>
+          </HStack>
+          <HStack justifyContent={"space-between"}>
+            <Text>Shipping</Text>
+            <Heading size={"sm"}>$19.99</Heading>
+          </HStack>
+          <HStack justifyContent={"space-between"}>
+            <Text>Taxes (estimated)</Text>
+            <Heading size={"sm"}>$23.80</Heading>
+          </HStack>
+        </VStack>
       </VStack>
+      <Divider borderColor={"lightgray"} />
+      <HStack justifyContent={"space-between"} w={"full"}>
+        <Text>Total</Text>
+        <Heading size={"lg"}>$162.79</Heading>
+      </HStack>
     </VStack>
-    <Divider borderColor={"lightgray"} />
-    <HStack justifyContent={"space-between"} w={"full"}>
-      <Text>Total</Text>
-      <Heading size={"lg"}>$162.79</Heading>
-    </HStack>
-  </VStack>
-);
+  );
+};
 
 export default Cart;
